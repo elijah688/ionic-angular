3f1f81bd865b1f0ceb1bd3b946f5dbb1340cb08e
elijah688 <elijah.iliev.95@gmail.com>
2019-12-06 23:59:52 +0000

    feat(recipe.service): generated recipes service with the ionic cli

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipesService } from './recipes.service';
 
 @Component({
   selector: 'app-recipes',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesPage implements OnInit {
 
-  constructor() { }
+  constructor(private recipeServ:RecipesService) { }
 
   ngOnInit() {
+
   }
 
 }
---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+  readonly swapiURL:string = "https://swapi.co/api/people"
+  constructor(private http: HttpClient) { }
+
+  getPeople(){
+    this.http.get(this.swapiURL).subscribe(people=>{
+      console.log(people);
+    })
+  }
+}
