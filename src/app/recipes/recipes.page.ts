079acb99782229c6cc7b44a6b450eb24950bcceb
elijah688 <elijah.iliev.95@gmail.com>
2019-12-07 06:12:11 +0000

    refactor(recipes.model): changed SWAPI for rickAndMortyAPI

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RecipesService } from './recipes.service';
+import { Recipe } from './recipes.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.sass'],
 })
-export class RecipesPage implements OnInit {
+export class RecipesPage implements OnInit, OnDestroy {
+  
+  private recipes:Recipe[] = [];
 
+  private revipesSubscription:Subscription = new Subscription();
   constructor(private recipeServ:RecipesService) { }
 
   ngOnInit() {
-    this.recipeServ.getPeople();
+    this.recipeServ.getRecipes();
+    this.revipesSubscription = this.recipeServ.recipesSubject.subscribe(recipes=>{
+      this.recipes = recipes;
+    });
+  }
+  ngOnDestroy(): void {
+    this.revipesSubscription.unsubscribe();
   }
 
 }
--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,16 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
+import { Recipe } from './recipes.model';
+import { Subject, Observable } from 'rxjs';
+
+interface rickAndMortyData {
+  info:{
+    count: number,
+    next: string,
+    pages: number,
+    prev: string,
+  },
+  results:Recipe[] 
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipesService {
-  readonly swapiURL:string = "https://swapi.co/api/people"
+  readonly swapiURL:string = "https://rickandmortyapi.com/api/character"
+  private _recipeSubject:Subject<Recipe[]> = new Subject<Recipe[]>();
   constructor(private http: HttpClient) { }
 
-  getPeople(){
-    this.http.get(this.swapiURL).subscribe(people=>{
-      console.log(people);
+  getRecipes():void{
+    this.http.get<rickAndMortyData>(this.swapiURL).subscribe(recipes=>{
+      this._recipeSubject.next(recipes.results);
+      console.log(recipes)
     })
+  }
+
+  get recipesSubject():Observable<Recipe[]>{
+    return this._recipeSubject.asObservable();
   }
 }
